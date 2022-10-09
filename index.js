fca62391655996d8b781011e637c8b4700f97f0f
Stefan Fitoussi <stefan.fit@web.de>
2022-10-09 17:53:08 +0200

    add create cards section

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const bookmarkButton = document.querySelector('[data-js="bookmark"]');
+const bookmarkButton = document.querySelector('div[data-js="bookmark"]');
+
 bookmarkButton.addEventListener("click", () => {
   bookmarkButton.classList.toggle("bookmark--active");
 });
+
+console.log(bookmarkButton);
 
 const answerButton = document.querySelector('[data-js="answerButton"]');
 const answerP = document.querySelector('[data-js="answerP"]');
@@ -14,17 +17,4 @@ answerButton.addEventListener("click", () => {
     answerP.classList.remove("card__answer--active");
     answerButton.textContent = "Show answer";
   }
-});
-
-const form = document.querySelector('[data-js="form"]');
-const inputQuestion = document.querySelector('[data-js="input-question"]');
-const inputAnswer = document.querySelector('[data-js="input-answer"]');
-const inputTag = document.querySelector('[data-js="input-tag"]');
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const formElements = event.target.elements;
-  const formData = new FormData(event.target);
-  const data = Object.fromEntries(formData);
 });
