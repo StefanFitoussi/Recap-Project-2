39e510c63bd6301f53af3f70ccaa8c3ca46ab21e
Stefan Fitoussi <stefan.fit@web.de>
2022-10-09 11:39:00 +0200

    add data-js

--- index.js
+++ index.js
@@ -15,3 +15,16 @@ answerButton.addEventListener("click", () => {
     answerButton.textContent = "Show answer";
   }
 });
+
+const form = document.querySelector('[data-js="form"]');
+const inputQuestion = document.querySelector('[data-js="input-question"]');
+const inputAnswer = document.querySelector('[data-js="input-answer"]');
+const inputTag = document.querySelector('[data-js="input-tag"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formElements = event.target.elements;
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+});
